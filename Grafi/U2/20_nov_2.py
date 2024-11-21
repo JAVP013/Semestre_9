40e41506b4dfb801862356ca0b843f1ef2d3a5ee
JAVP013 <joalvaldezpo@ittepic.edu.mx>
2024-11-21 13:12:42 -0700

    act 20 niov

---
+++ Grafi/U2/20_nov_2.py
@@ -0,0 +1,118 @@
+import glfw
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np 
+import time 
+
+TM = 600 # pixeles
+TC = 10  # unidades
+
+class Cruz:
+    def __init__(self):
+        self.vertices = np.array([[0.5,1.5],
+                                  [0.5,0.5],
+                                  [1.5,0.5],
+                                  [1.5,-0.5],
+                                  [0.5,-0.5],
+                                  [0.5,-2.5],
+                                  [-0.5,-2.5],
+                                  [-0.5,-0.5],
+                                  [-1.5,-0.5],
+                                  [-1.5,0.5],
+                                  [-0.5,0.5],
+                                  [-0.5,1.5]],dtype=np.float32)
+    def trazar(self):
+        glBegin(GL_LINE_LOOP)
+        for vertice in self.vertices:
+            glVertex2fv(vertice)
+        glEnd()
+    def transformar(self,M):
+        i = 0
+        while i<len(self.vertices):
+            P = np.matrix([[self.vertices[i][0]],
+                          [self.vertices[i][1]],
+                          [1]],dtype=np.float32)
+            Pp = np.dot(M,P)
+            self.vertices[i][0] = Pp[0]
+            self.vertices[i][1] = Pp[1]
+            i+=1
+    def trasladar(self,tx,ty):
+        T = np.array([[1,0,tx],
+                      [0,1,ty],
+                      [0,0,1]],dtype=np.float32)
+        self.transformar(T)
+    def escalar(self,sx,sy):
+        S = np.array([[sx,0,0],
+                      [0,sy,0],
+                      [0,0,1]],dtype=np.float32)
+        self.transformar(S)
+    def rotar(self,theta):
+        t = np.radians(theta)
+        R = np.array([[np.cos(t),-np.sin(t),0],
+                      [np.sin(t),np.cos(t),0],
+                      [0,0,1]],dtype=np.float32)
+        self.transformar(R)
+    def rotar_xy(self,theta,xr,yr):
+        t = np.radians(theta)
+        m = np.array([[np.cos(t),-np.sin(t),xr*(1-np.cos(t))+yr*np.sin(t)],
+                      [np.sin(t),np.cos(t),yr*(1-np.cos(t))-xr*np.sin(t)],
+                      [0,0,1]],dtype=np.float32)
+        self.transformar(m)
+
+
+      
+
+
+def main():
+    
+    if not glfw.init():
+        return
+
+    ventana = glfw.create_window(TM, TM, "20400833", None, None)
+    if not ventana:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(ventana)
+    glClearColor(0, 0, 0, 0)
+    gluOrtho2D(-TC, TC, -TC, TC)
+   
+    c1=Cruz()
+    c2=Cruz()
+      
+   
+    while not glfw.window_should_close(ventana):
+        glfw.poll_events()
+        glClear(GL_COLOR_BUFFER_BIT)
+        
+        glColor(1, 1, 1)
+        glBegin(GL_LINES)
+        glVertex2f(-TC, 0)
+        glVertex2f(TC, 0)
+        glVertex2f(0, -TC)
+        glVertex2f(0, TC)
+        glEnd()
+        
+        glColor(1, 0, 0)
+
+        c1.trazar()
+
+        glColor(0, 1, 0)
+
+        c2.trazar()
+       
+        time.sleep(0.1)
+        
+        c2.rotar_xy(45,0.5,1.5)
+        
+        
+        
+
+
+
+        glfw.swap_buffers(ventana)
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
